1e56f8122d75dd0985fe88d815d6bae3735ad780
Halidg <halid2323@icloud.com>
2022-02-22 01:24:26 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="container">
+   <InputRange v-model="inputValue"/>
+   <div class="block">
+    <button class="button" @click="inputValue=25">25%</button>
+    <button class="button" @click="inputValue=50">50%</button>
+    <button class="button" @click="inputValue=75">75%</button>
+    <button class="button" @click="inputValue=100">100%</button>     
   </div>
-  <router-view/>
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+
+import InputRange from './components/InputRange.vue'
+
+export default {
+  components:{InputRange},
+  data() {
+    return {
+      inputValue: 15
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.container {
+  margin: 0 auto;
+  padding: 15px;
+  max-width: 900px;
 }
 
-#nav {
-  padding: 30px;
+.block {
+  display: flex;
+  justify-content: space-between;
+  width: 30%;
+  margin: 0 auto;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+.button {
+  border-radius: 22px;
+  border: none;
+  padding: 10px 10px;
+  color: white;
+  background: #54618b;
+  &:hover {
+    cursor: pointer;
   }
 }
 </style>
